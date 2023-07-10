42f0598e67ab9cedeb669ef0e1167a340b3d404c
Michael <michaelcuasi@gmail.com>
2023-07-10 08:19:25 +0400

    useContext 1

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,56 +1,52 @@
 <script>
-  // import { onDestroy } from "svelte";
-  // import settings from "../stores/settings";
-  // import location from "../stores/location";
+  let values = { username: '', email: '', password: '' };
+  let errors = {};
+  let isSubmitting = false;
 
-  // const unsubscribe = settings.subscribe((settings) => {
-  //   console.log('home subscribe')
-  //   colorScheme = settings.colorScheme
-  // })
-
-  // onDestroy(unsubscribe)
-
-    import { writable } from "svelte/store";
-    import { tweened } from "svelte/motion";
-    import { cubicOut } from "svelte/easing";
-   
-    const size = tweened({width: 100, height: 100}, {duration: 500, easing: cubicOut })
-    
-
-
+  function validate() {
+    const errors = {};
+    if (!values.username) {
+      errors.username = 'username is required';
+    }
+    if (!values.email) {
+      errors.email = 'email is required';
+    }
+    if (!values.password) {
+      errors.password = 'password is required';
+    }
+    if (
+      values.email &&
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)
+    ) {
+      errors.email = 'the email is invalid';
+    }
+    return errors;
+  }
 </script>
 
-<!-- <button on:click={() => {$scale += 1}}>
-   Make it bigger
-</button>
+<form
+  on:submit|preventDefault={() => {
+    errors = validate();
+    if (Object.keys(errors).length === 0) {
+      isSubmitting = true;
+      setTimeout(() => {
+        alert(values.username);
+        isSubmitting = false;
+      }, 2000);
+    }
+  }}
+>
+  <label for="username">Username</label> <br />
+  <input type="text" bind:value={values.username} name="username" /><br />
+  {#if errors.username} <p>{errors.username}</p> {/if}
 
-<button on:click={() => {$scale -= 1}}>
-  Make it smaller
-</button> -->
+  <label for="email">Email</label><br />
+  <input type="email" bind:value={values.email} name="email" /><br />
+  {#if errors.email} <p>{errors.email}</p> {/if}
 
-<button on:click={async () => {
-    await size.set({
-      width: Math.random()*500,
-      height: Math.random()*500,
-    })
-    console.log('done')
-  }}>
-  random size
-</button>
+  <label for="password">Password</label><br />
+  <input type="password" bind:value={values.password} name="password" /><br />
+  {#if errors.password} <p>{errors.password}</p> {/if}
 
-<div 
-  style="width: {$size.width}px; height: {$size.height}px; background-color: purple;">
-
-</div>
-
-<!-- <h2>Home</h2>
-
-{#if $location && !$location.error}
-  {$location.latitude}, {$location.latitude}
-{/if}
-
-{#if $location && $location.error}
-  {$location.error.message}
-{/if}
-
-{$settings.colorScheme} -->
+  <button type="submit" disabled={isSubmitting}>Submit</button>
+</form>
